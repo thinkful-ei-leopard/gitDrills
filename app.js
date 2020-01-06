1aded14e0e0000304f26c01d75ec5ce011446a46
Jose <jozylopez27@gmail.com>
2020-01-06 15:10:20 -0600

    created func

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+/* eslint-disable strict */
 const myTeam = 'Jose & Jorge';
 
 console.log(myTeam);
 
+function createGreeting() {
 console.log(`'Hi, my name is Jose and I'm 23 years old'`)
+}
+
+createGreeting();
