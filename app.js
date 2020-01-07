c76d69be33c47a4a5d15c7d033da0a23b24608fe
jorgeespino  Student <selectjorge@gmail.com>
2020-01-06 19:50:11 -0600

    fun drill 2

--- app.js
+++ app.js
@@ -3,23 +3,27 @@
 
 // console.log(myTeam);
 
-function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age can not be negative');
-  }
-  return 2020 - age;
+// function getYearOfBirth(age) {
+//   if (age < 0) {
+//     throw new Error('Age can not be negative');
+//   }
+//   return 2020 - age;
+// }
+
+// function createGreeting(name, age) {
+//   const yob = getYearOfBirth(age);
+//   return `'Hi, my name is ${name} and I'm ${age} years old.' 
+//   'I was born in ${yob}'`;
+// }
+
+
+// try {
+//   const greeting1 = createGreeting(24);
+// } catch(e) {
+//   console.error(e);
+// }
+
+function jediName(firstName,lastName) {
+return lastName.split('').slice(0, 3).join('') + firstName.split('').slice(0, 2).join('')
 }
-
-function createGreeting(name, age) {
-  const yob = getYearOfBirth(age);
-  return `'Hi, my name is ${name} and I'm ${age} years old.' 
-  'I was born in ${yob}'`;
-}
-
-
-try {
-  const greeting1 = createGreeting(24);
-} catch(e) {
-  console.error(e);
-}
-
+console.log(jediName('Jorge','Espino'))
